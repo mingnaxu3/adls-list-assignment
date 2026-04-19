fb766da533fab339a5d6d2840dd38faeeb5edbeb
Mingna Xu <mingnaxu3@gmail.com>
2026-04-19 14:07:50 -0400

    Implemented filtering algorithm

--- adls_practice/list_contents.py
+++ adls_practice/list_contents.py
@@ -13,9 +13,19 @@ def main():
     # List all blobs in the container
     container_client = blob_service_client.get_container_client(container_name)
 
+
+    #filter for the files ending in '.csv.gz'
+    #the list is container_client.list_blobs()
+    
+
     print(f"Listing files in container: {container_name}")
     for blob in container_client.list_blobs():
-        print(blob.name)
+        #blob is a BlobProperties element 
+        #the name of the blob is a string so can just python str function, endswith()
+
+        #do i need to actually modify the list or can i just filter it out 
+        if blob.name.endswith(".csv.gz"):
+          print(blob.name)
 
 if __name__ == "__main__":
     main()
